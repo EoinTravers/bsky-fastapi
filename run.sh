7e355ed6aef69e21f4011dc159f5ec5f871fba4d
Eoin Travers <eoin.travers@gmail.com>
2025-02-06 16:45:36 +0000

    fix: ports

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
-if [ "$ENV" = "PROD" ]; then
-    uvicorn app.main:app --port 10000
-else
-    uvicorn app.main:app --port 8080
+if [ -z "$PORT" ]; then
+    if [ "$ENV" = "PROD" ]; then
+        PORT=80
+    else
+        PORT=9999
+    fi
 fi
+
+uvicorn app.main:app --host 0.0.0.0 --port $PORT
